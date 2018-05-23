884db03c34b1cd9e6f4b6469648eec5418c01bbe
Naveen-Surya <38528237+Naveen-Surya@users.noreply.github.com>
2018-05-24 02:31:21 +0530

    Update parallel.c

--- Assignment4/parallel.c
+++ Assignment4/parallel.c
@@ -45,3 +45,21 @@ void Programm_Stop(char *txt)
    MPI_Finalize();
    exit(1);
 }
+
+void init_parallel(int iproc,
+               int jproc,
+               int imax,
+               int jmax,
+               int *myrank,
+               int *il,
+               int *ir,
+               int *jb,
+               int *jt,
+               int *rank_l,
+               int *rank_r,
+               int *rank_b,
+               int *rank_t,
+               int *omg_i,
+               int *omg_j,
+               int num_proc)
+{}
