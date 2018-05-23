61b1d4472d9be5be17d2898fad37bad09011f2a6
Naveen-Surya <38528237+Naveen-Surya@users.noreply.github.com>
2018-05-24 02:29:26 +0530

    Update parallel.h

--- Assignment4/parallel.h
+++ Assignment4/parallel.h
@@ -15,3 +15,20 @@ void Programm_Sync(char *txt);
 void Programm_Stop(char *txt);
 /* all processes will produce a text output, be synchronized and finished */
 
+void init_parallel(int iproc,
+					int jproc,
+					int imax,
+					int jmax,
+					int *myrank,
+					int *il,
+					int *ir,
+					int *jb,
+					int *jt,
+					int *rank_l,
+					int *rank_r,
+					int *rank_b,
+					int *rank_t,
+					int *omg_i,
+					int *omg_j,
+					int num_proc);
+
